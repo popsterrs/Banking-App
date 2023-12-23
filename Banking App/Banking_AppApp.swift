e62b0166f6458629064a1626317c86cb00b62c80
popsterrs <jackpadbury1@icloud.com>
2023-12-23 15:12:17 +0000

    Initial Commit

---
+++ Banking App/Banking_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Banking_AppApp.swift
+//  Banking App
+//
+//  Created by Jack Padbury on 23/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Banking_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
